e4d22b92ffd6d1343f1eeb89fc6e59f88bf41aca
liumei <liumeishengqi@huluwa.cc>
2017-05-11 15:57:36 +0800

    删除一条记录

--- page/deleteDnsRecord.php
+++ page/deleteDnsRecord.php
@@ -8,3 +8,29 @@
  * Time: 3:42 PM
  */
 
+
+require_once '../vendor/autoload.php';
+
+require_once '../load.php';
+
+$conn = \liumapp\dns\models\db::getInstance();
+
+$queryBuilder = $conn->createQueryBuilder();
+
+$uid = 1;
+
+$domainId = 1;
+
+$queryBuilder->delete('lmdns')
+    ->where('id = :record_id')
+    ->andWhere('uid = :uid')
+    ->andWhere('domainId = :domainId')
+    ->setParameter(':record_id', addslashes($_POST['id']))
+    ->setParameter(':uid' , $uid)
+    ->setParameter(':domainId' , $domainId)
+    ->execute();
+
+echo $queryBuilder->execute();
+
+
+
